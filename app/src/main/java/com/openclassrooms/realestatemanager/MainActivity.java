d639eb50ac35bc9af1e4d75d9441f602d0f78b0a
PaulineDev-Code <p.cellier91@gmail.com>
2023-09-25 02:59:47 +0200

    Initial commit

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -0,0 +1,43 @@
+package com.openclassrooms.realestatemanager;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewMain;
+    private TextView textViewQuantity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
+        this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
+
+        this.configureTextViewMain();
+        this.configureTextViewQuantity();
+    }
+
+    private void configureTextViewMain(){
+        //Replaced textViewMain's findViewById by the one from activity_main's layout
+        //BuildConfig2 file deleted as same file already existed
+        this.textViewMain.setTextSize(15);
+        this.textViewMain.setText("Le premier bien immobilier enregistré vaut ");
+    }
+
+    private void configureTextViewQuantity(){
+        int quantity = Utils.convertDollarToEuro(100);
+        this.textViewQuantity.setTextSize(20);
+        //Fixed setText function (Int to String)
+        /*this.textViewQuantity.setText(Integer.toString(quantity));*/
+        this.textViewQuantity.setText(String.valueOf(quantity));
+    }
+
+
+}
